c77109f51bce475df51327016b8ae58c4abe1416
MokoMaracucho <desmoulin.federico@gmail.com>
2021-06-07 16:31:56 +0200

    First part : materials

--- src/app/page-001/services/page-001.service.ts
+++ src/app/page-001/services/page-001.service.ts
@@ -17,6 +17,8 @@ export class Page001Service {
   private engine: BABYLON.Engine;
   private scene: BABYLON.Scene;
 
+  private hdrTexture: BABYLON.CubeTexture;
+
   private camera: BABYLON.UniversalCamera;
 
   private light: BABYLON.Light;
@@ -43,6 +45,14 @@ export class Page001Service {
 
     this.scene = new BABYLON.Scene(this.engine);
 
+    /* this.scene.debugLayer.show(); */
+
+    // HDR
+
+    this.hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("../../assets/env/environment.env", this.scene);
+    this.hdrTexture.rotationY = Math.PI/2;
+    this.scene.environmentTexture = this.hdrTexture;
+
     this.camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(0, 0, 0), this.scene);
     this.camera.position = new BABYLON.Vector3(10, 10, 10);
     this.camera.target = new BABYLON.Vector3(0, 0, 0);
@@ -51,82 +61,130 @@ export class Page001Service {
     this.light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), this.scene);
     this.light.intensity = 1;
 
-    var material = new BABYLON.StandardMaterial("material", this.scene);
-    material.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    const Panel_front_1_MATERIAL = new BABYLON.PBRMaterial("Panel_front_1_MATERIAL", this.scene);
+    Panel_front_1_MATERIAL.albedoColor = new BABYLON.Color3(0, 0, 0);
+    Panel_front_1_MATERIAL.metallic = 0.3;
+    Panel_front_1_MATERIAL.roughness = 0;
+    Panel_front_1_MATERIAL.alpha = 0.5;
+
+    const Panel_front_2_MATERIAL = new BABYLON.PBRMaterial("Panel_front_2_MATERIAL", this.scene);
+    Panel_front_2_MATERIAL.albedoColor = new BABYLON.Color3(0, 0, 0);
+    Panel_front_2_MATERIAL.metallic = 0.3;
+    Panel_front_2_MATERIAL.roughness = 0;
+
+    const Plastik_glanec_MATERIAL = new BABYLON.PBRMaterial("Plastik_glanec_MATERIAL", this.scene);
+    Plastik_glanec_MATERIAL.albedoColor = new BABYLON.Color3(0, 0, 0);
+    Plastik_glanec_MATERIAL.metallic = 0.1;
+    Plastik_glanec_MATERIAL.roughness = 0.8;
+
+    const Chrom_Kant_MATERIAL = new BABYLON.PBRMaterial("Chrom_Kant_MATERIAL", this.scene);
+    Chrom_Kant_MATERIAL.albedoColor = new BABYLON.Color3(1, 1, 1);
+    Chrom_Kant_MATERIAL.metallic = 1;
+    Chrom_Kant_MATERIAL.roughness = 0.1;
+
+    const Chrom_rem_MATERIAL = new BABYLON.PBRMaterial("Chrom_rem_MATERIAL", this.scene);
+    Chrom_rem_MATERIAL.albedoColor = new BABYLON.Color3(1, 1, 1);
+    Chrom_rem_MATERIAL.metallic = 1;
+    Chrom_rem_MATERIAL.roughness = 0.1;
+
+    const Screen_off_MATERIAL = new BABYLON.StandardMaterial("Screen_off_MATERIAL", this.scene);
+    const Screen_off_MATERIAL_TEXTURE = new BABYLON.VideoTexture("video", "../../assets/videos/pi.mp4", this.scene);
+    Screen_off_MATERIAL.diffuseTexture = Screen_off_MATERIAL_TEXTURE;
+    Screen_off_MATERIAL.emissiveColor = BABYLON.Color3.White();
+
+    const Chrom_MATERIAL = new BABYLON.PBRMaterial("Chrom_MATERIAL", this.scene);
+    Chrom_MATERIAL.albedoColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+    Chrom_MATERIAL.metallic = 1;
+    Chrom_MATERIAL.roughness = 0.2;
+
+    const Metall_strip_MATERIAL = new BABYLON.PBRMaterial("Metall_strip_MATERIAL", this.scene);
+    Metall_strip_MATERIAL.albedoColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+    Metall_strip_MATERIAL.metallic = 1;
+    Metall_strip_MATERIAL.roughness = 0.2;
+    Metall_strip_MATERIAL.bumpTexture = new BABYLON.Texture("../../assets/img/textures/Metal_Brushed_Rough.jpg", this.scene);
+
+    const Plastic_black_MATERIAL = new BABYLON.PBRMaterial("Plastic_black_MATERIAL", this.scene);
+    Plastic_black_MATERIAL.albedoColor = new BABYLON.Color3(0, 0, 0);
+    Plastic_black_MATERIAL.metallic = 0.1;
+    Plastic_black_MATERIAL.roughness = 0.8;
 
     BABYLON.SceneLoader.ImportMesh("", "../../assets/gltf/page-001/", "Samsung_SMART_TV.gltf", this.scene, function (meshes) {
       const Key_power = meshes[1];
-      Key_power.material = material;
+      /* Key_power.material = material; */
 
       const Panel_front_1 = meshes[2];
-      Panel_front_1.material = material;
+      Panel_front_1.material = Panel_front_1_MATERIAL;
 
       const Panel_front_2 = meshes[3];
-      Panel_front_2.material = material;
+      Panel_front_2.material = Panel_front_2_MATERIAL;
 
       const Remote_Steel_ward = meshes[4];
-      Remote_Steel_ward.material = material;
+      /* Remote_Steel_ward.material = material; */
 
       const Phone_opas = meshes[5];
-      Phone_opas.material = material;
+      /* Phone_opas.material = material; */
 
       const Plastik_glanec = meshes[6];
-      Plastik_glanec.material = material;
+      Plastik_glanec.material = Plastik_glanec_MATERIAL;
 
       const Panel_back = meshes[7];
-      Panel_back.material = material;
+      /* Panel_back.material = material; */
 
       const Chrom_Kant = meshes[8];
-      Chrom_Kant.material = material;
+      Chrom_Kant.material = Chrom_Kant_MATERIAL;
 
       const Remote_screen = meshes[9];
-      Remote_screen.material = material;
+      /* Remote_screen.material = material; */
 
       const Chrom_rem = meshes[10];
-      Chrom_rem.material = material;
+      Chrom_rem.material = Chrom_rem_MATERIAL;
 
       const Back_label = meshes[11];
-      Back_label.material = material;
+      /* Back_label.material = material; */
 
       const Screen_off = meshes[12];
-      Screen_off.material = material;
+      Screen_off.material = Screen_off_MATERIAL;
 
       const Chrom = meshes[13];
-      Chrom.material = material;
+      Chrom.material = Chrom_MATERIAL;
 
       const Metall_strip = meshes[14];
-      Metall_strip.material = material;
+      Metall_strip.material = Metall_strip_MATERIAL;
 
       const Glass_black = meshes[15];
-      Glass_black.material = material;
+      /* Glass_black.material = material; */
 
       const Metall_dark = meshes[16];
-      Metall_dark.material = material;
+      /* Metall_dark.material = material; */
 
       const Gold = meshes[17];
-      Gold.material = material;
+      /* Gold.material = material; */
 
       const Steel = meshes[18];
-      Steel.material = material;
+      /* Steel.material = material; */
 
       const Plastic_black = meshes[19];
-      Plastic_black.material = material;
+      Plastic_black.material = Plastic_black_MATERIAL;
 
       const Plastic_white = meshes[20];
-      Plastic_white.material = material;
+      /* Plastic_white.material = material; */
 
       const Plastic_yellow = meshes[21];
-      Plastic_yellow.material = material;
+      /* Plastic_yellow.material = material; */
 
       const Luminium = meshes[22];
-      Luminium.material = material;
+      /* Luminium.material = material; */
 
       const Plastic_black_gloss = meshes[23];
-      Plastic_black_gloss.material = material;
+      /* Plastic_black_gloss.material = material; */
 
       const Plastic_black_key = meshes[24];
-      Plastic_black_key.material = material;
+      /* Plastic_black_key.material = material; */
     });
+
+
+
+    Screen_off_MATERIAL_TEXTURE.video.play();
 
     // AXIS
 
